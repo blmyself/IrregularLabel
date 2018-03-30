af8bccb86b8481a7bed985f701b8261e37ae6e1e
caiqiang <caiqiang@kuaijiankang.com>
2018-03-30 15:11:39 +0800

    Initial Commit

---
+++ IrregularLabel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IrregularLabel
+//
+//  Created by 蔡强 on 2018/3/30.
+//  Copyright © 2018年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
